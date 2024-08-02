5848bd3be052a6ccc3ac236d2eb59221b3b8188f
Meet Soni <98331817+Meetsoni3591@users.noreply.github.com>
2024-08-02 15:42:23 +0530

    create exceptionexmaple
    exception examples

---
+++ src/exceptionexample/ExceptionDemo.java
@@ -0,0 +1,20 @@
+package exceptionexample;
+import java.util.*;
+public class ExceptionDemo {
+	
+	public static void main(String args[]) {
+		int a,b,c;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter first number ");
+		a=sc.nextInt();
+		System.out.println("enter second number ");
+		b=sc.nextInt();
+		try {
+			c=a/b;
+			System.out.println("the result is "+ c);
+		}catch(ArithmeticException arr){
+			System.out.println(arr.getMessage());
+		}
+	}
+
+}
